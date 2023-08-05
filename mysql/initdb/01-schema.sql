bb34a891578d8290d77ea9aa5f5235708bf8b154
gora <ehaakdl@naver.com>
2023-08-05 12:41:01 +0900

    feature: 스키마 추가

--- mysql/initdb/01-schema.sql
+++ mysql/initdb/01-schema.sql
@@ -1,5 +1,89 @@
 create database IF NOT EXISTS gora;
 
 use gora;
+create table privilege
+(
+    seq          bigint auto_increment
+        primary key,
+    display_name varchar(255)                        not null,
+    code         varchar(255)                        not null comment '식별용도',
+    created_at   timestamp default CURRENT_TIMESTAMP not null,
+    updated_at   timestamp default CURRENT_TIMESTAMP not null,
+    deleted_at   timestamp                           null,
+    constraint privilege_pk
+        unique (code)
+);
 
+create table role
+(
+    seq          bigint auto_increment
+        primary key,
+    display_name varchar(255)                        null,
+    code         varchar(255)                        not null comment '식별용도',
+    created_at   timestamp default CURRENT_TIMESTAMP not null,
+    updated_at   timestamp default CURRENT_TIMESTAMP not null,
+    deleted_at   timestamp                           null,
+    constraint role_pk
+        unique (code)
+);
+
+create table role_privilege
+(
+    privilege_seq bigint                              not null,
+    role_seq      bigint                              not null,
+    created_at    timestamp default CURRENT_TIMESTAMP not null,
+    updated_at    timestamp default CURRENT_TIMESTAMP not null,
+    primary key (privilege_seq, role_seq),
+    constraint role_privilege_privilege_seq_fk
+        foreign key (privilege_seq) references privilege (seq),
+    constraint role_privilege_role_seq_fk
+        foreign key (role_seq) references role (seq)
+);
+
+create table social_user
+(
+    seq         bigint auto_increment
+        primary key,
+    social_type varchar(20)                         not null comment '구글(google)',
+    created_at  timestamp default CURRENT_TIMESTAMP null,
+    updated_at  timestamp default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    deleted_at  timestamp                           null
+);
+
+create table token
+(
+    seq              bigint auto_increment
+        primary key,
+    access           varchar(255) not null,
+    access_expire_at timestamp    not null,
+    refresh          varchar(255) not null,
+    user_seq         bigint       not null
+);
+
+create table user
+(
+    seq        bigint auto_increment
+        primary key,
+    email      varchar(255)                         not null,
+    password   varchar(255)                         null,
+    type       varchar(20)                          not null comment 'basic(일반), 소셜가입자(social)',
+    created_at timestamp  default CURRENT_TIMESTAMP not null,
+    updated_at timestamp  default CURRENT_TIMESTAMP not null,
+    deleted_at timestamp                            null,
+    social_seq bigint                               null,
+    disable    tinyint(1) default 0                 null,
+    constraint user_social_user_seq_fk
+        foreign key (social_seq) references social_user (seq)
+);
+
+create table user_role
+(
+    role_seq bigint not null,
+    user_seq bigint not null,
+    primary key (role_seq, user_seq),
+    constraint user_role_role_seq_fk
+        foreign key (role_seq) references role (seq),
+    constraint user_role_user_seq_fk
+        foreign key (user_seq) references user (seq)
+);
 
