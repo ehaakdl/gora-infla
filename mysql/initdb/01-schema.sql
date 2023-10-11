48cdbb496e118a28a358b2d7ded70a4059024f7a
mosehun <ehaakdl@naver.com>
2023-10-11 13:18:45 +0900

    refactor

--- mysql/initdb/01-schema.sql
+++ mysql/initdb/01-schema.sql
@@ -12,22 +12,40 @@ CREATE TABLE `email_verify` (
   PRIMARY KEY (`seq`)
 );
 
-CREATE TABLE `token` (
+
+-- gora.social_user definition
+
+CREATE TABLE `social_user` (
   `seq` bigint NOT NULL AUTO_INCREMENT,
-  `access` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `access_expire_at` timestamp NOT NULL,
-  `refresh` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `user_seq` bigint DEFAULT NULL,
-  `email_verify_seq` bigint DEFAULT NULL,
-  `type` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`seq`),
-  UNIQUE INDEX `access` (`access`),
-	UNIQUE INDEX `refresh` (`refresh`)
+  `social_type` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '구글(google)',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `created_by` bigint NOT NULL DEFAULT (-(1)),
+  `updated_by` bigint NOT NULL DEFAULT (-(1)),
+  `deleted_by` bigint DEFAULT NULL,
+  PRIMARY KEY (`seq`)
 );
 
-ALTER TABLE `token`
-ADD KEY `token_FK` (`email_verify_seq`),
-ADD CONSTRAINT `token_FK` FOREIGN KEY (`email_verify_seq`) REFERENCES `email_verify` (`seq`);
+-- gora.`user` definition
+
+CREATE TABLE `user` (
+  `seq` bigint NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `type` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'basic(일반), 소셜가입자(social)',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `social_seq` bigint DEFAULT NULL,
+  `created_by` bigint NOT NULL DEFAULT (-(1)),
+  `updated_by` bigint NOT NULL DEFAULT (-(1)),
+  `deleted_by` bigint DEFAULT NULL,
+  PRIMARY KEY (`seq`),
+  UNIQUE KEY `email` (`email`) USING BTREE,
+  KEY `user_social_user_seq_fk` (`social_seq`),
+  CONSTRAINT `user_social_user_seq_fk` FOREIGN KEY (`social_seq`) REFERENCES `social_user` (`seq`)
+);
 
 -- gora.privilege definition
 
@@ -62,25 +80,7 @@ CREATE TABLE `role` (
   UNIQUE KEY `role_pk` (`code`)
 );
 
--- gora.social_user definition
-
-CREATE TABLE `social_user` (
-  `seq` bigint NOT NULL AUTO_INCREMENT,
-  `social_type` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '구글(google)',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  `created_by` bigint NOT NULL DEFAULT (-(1)),
-  `updated_by` bigint NOT NULL DEFAULT (-(1)),
-  `deleted_by` bigint DEFAULT NULL,
-  PRIMARY KEY (`seq`)
-);
-
-
-
-
 -- gora.role_privilege definition
-
 CREATE TABLE `role_privilege` (
   `seq` bigint NOT NULL AUTO_INCREMENT,
   `role_seq` bigint NOT NULL,
@@ -90,29 +90,11 @@ CREATE TABLE `role_privilege` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`seq`),
-  UNIQUE KEY `role_privilege_UN` (`role_seq`,`privilege_seq`)
-); 
-
--- gora.`user` definition
-
-CREATE TABLE `user` (
-  `seq` bigint NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `type` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'basic(일반), 소셜가입자(social)',
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  `social_seq` bigint DEFAULT NULL,
-  `created_by` bigint NOT NULL DEFAULT (-(1)),
-  `updated_by` bigint NOT NULL DEFAULT (-(1)),
-  `deleted_by` bigint DEFAULT NULL,
-  PRIMARY KEY (`seq`),
-  UNIQUE KEY `email` (`email`) USING BTREE,
-  KEY `user_social_user_seq_fk` (`social_seq`),
-  CONSTRAINT `user_social_user_seq_fk` FOREIGN KEY (`social_seq`) REFERENCES `social_user` (`seq`)
+  UNIQUE KEY `role_privilege_UN` (`role_seq`,`privilege_seq`),
+  KEY `role_privilege_FK_1` (`privilege_seq`),
+  CONSTRAINT `role_privilege_FK` FOREIGN KEY (`role_seq`) REFERENCES `role` (`seq`),
+  CONSTRAINT `role_privilege_FK_1` FOREIGN KEY (`privilege_seq`) REFERENCES `privilege` (`seq`)
 );
-
 
 -- gora.user_role definition
 
@@ -127,3 +109,23 @@ CREATE TABLE `user_role` (
   CONSTRAINT `user_role_FK_1` FOREIGN KEY (`user_seq`) REFERENCES `user` (`seq`)
 );
 
+CREATE TABLE `token` (
+  `seq` bigint NOT NULL AUTO_INCREMENT,
+  `access` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `access_expire_at` timestamp NOT NULL,
+  `refresh` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_seq` bigint DEFAULT NULL,
+  `email_verify_seq` bigint DEFAULT NULL,
+  `type` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`seq`),
+  UNIQUE INDEX `access` (`access`),
+	UNIQUE INDEX `refresh` (`refresh`)
+);
+
+ALTER TABLE `token`
+ADD KEY `token_FK` (`email_verify_seq`),
+ADD CONSTRAINT `token_FK` FOREIGN KEY (`email_verify_seq`) REFERENCES `email_verify` (`seq`);
+
+ALTER TABLE `token`
+ADD KEY `token_FK_1` (`user_seq`),
+ADD CONSTRAINT `token_FK_1` FOREIGN KEY (`user_seq`) REFERENCES `user` (`seq`);
